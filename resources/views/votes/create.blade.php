73d7c45c849634f893ddbfaa0021b7f10040e00b
tenglongroy <tenglongroy@gmail.com>
2017-05-29 22:09:53 +1000

    finish votes/create page, functions of add/remove question and add/remove choice within question are working well.

--- resources/views/votes/create.blade.php
+++ resources/views/votes/create.blade.php
@@ -68,22 +68,22 @@
             return document.getElementById(tagName).childElementCount - 6;
         };
 
-        question_array = [];   //the value corresponds to each index's option number
+        question_index = 0;     //the value corresponds to each question's index
         var newDiv = '<div class="container" id="{@index}"><hr><h5>{@index}</h5>';
         newDiv += '<div class="row"><input type="text" name="{@index}_description" placeholder="question"/></div>';
         newDiv += '<div class="row"><input type="checkbox" id="{@index}_multiple" name="{@index}_multiple" value="1" /><h5>multiple</h5></div>';
         newDiv += '<div class="row" id="{@index}_max" style="display:none" ><h6>max choice</h6><input type="number" name="{@index}_max"/></div>';
         newDiv += '<div class="row"><input type="button" id="{@index}_add" value="Add"/>';
         newDiv += '<input type="button" id="{@index}_minus" value="Minus"/></div>';
-        newDiv += '<div class="row"><input type="text" name="{@index}_choice1" placeholder="{@index}_choice1"/></div>';
-        newDiv += '<div class="row"><input type="text" name="{@index}_choice2" placeholder="{@index}_choice2"/></div></div>';
+        newDiv += '<div class="row input" id="{@index}_choice1"><input type="text" name="{@index}_choice1" placeholder="{@index}_choice1"/></div>';
+        newDiv += '<div class="row input" id="{@index}_choice2"><input type="text" name="{@index}_choice2" placeholder="{@index}_choice2"/></div></div>';
 
 
         $(function() {
             $('#more_question_button').click(function(){
-                $('#vote_form').append(newDiv.replace(/{@index}/g, "question"+(question_array.length+1)));
+                $('#vote_form').append(newDiv.replace(/{@index}/g, "question"+(question_index+1)));
                 //event for multiple choice checkbox
-                $("#question"+(question_array.length+1)+"_multiple").change(function(){
+                $("#question"+(question_index+1)+"_multiple").change(function(){
                     var maxName = '#'+this.name.split('_')[0]+'_max';
                     if(this.checked){
                         $(maxName).show();
@@ -92,26 +92,29 @@
                         $(maxName).hide();
                     }
                 });
-
                 //event for add a choice
-                $("#question"+(question_array.length+1)+"_add").click(function(){
-                    var newInput = '<div class="row"><input type="text" name="{@index}" placeholder="{@index}"/></div>';
+                $("#question"+(question_index+1)+"_add").click(function(){
+                    var newInput = '<div class="row input" id="{@index}"><input type="text" name="{@index}" placeholder="{@index}"/></div>';
                     var idSplit = this.id.split('_')[0];
                     var newInputtemp = newInput.replace(/{@index}/g, idSplit+"_choice"+(countInputIndex(idSplit)+1));
                     console.log(this.id +" .."+ newInputtemp);
-                    $('#'+this.id.split('_')[0]).append(
-                        newInputtemp
-                    );
+                    $('#'+idSplit).append(newInputtemp);
+                });
+                //event for minus a choice
+                $("#question"+(question_index+1)+"_minus").click(function(){
+                    var idSplit = this.id.split('_')[0];
+                    console.log('remove #'+idSplit+'_choice'+countInputIndex(idSplit))
+                    $('#'+idSplit+'_choice'+countInputIndex(idSplit)).remove();
                 });
 
-                question_array.push(2);
+                question_index++;
             });
         });
         $(function() {
             $('#less_question_button').click(function(){
                 var oldDiv = '#{@index}';
-                $(oldDiv.replace("{@index}", "question"+(question_array.length))).remove();
-                question_array.pop();
+                $(oldDiv.replace("{@index}", "question"+question_index)).remove();
+                question_index--;
             });
         });
     </script>
